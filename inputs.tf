785fe0a6c3aeb6fe87e801767007ff43f454c9a7
Ratnaji <ratnajiss@gmail.com>
2022-07-07 16:08:18 +0530

    update

---
+++ inputs.tf
@@ -0,0 +1,20 @@
+variable "subscription_id" {
+  type = string
+  default ="318d4f61-55aa-474f-8b44-a39891607f82"
+  description="Iacsubscripton_id"
+}
+variable "client_id" {
+  type = string
+  default ="50d0dc4f-fb64-45bc-81d1-2692048e9746"
+  description="Iacclient_id"
+}
+variable "tenant_id" {
+  type = string
+  default ="eff34334-a764-4b0d-9968-ec33bec86048"
+  description="Iactenant_id"
+}
+variable "client_secret" {
+  type = string
+  default ="oz08Q~DwwbNovp81oZ48dvsv5RUGK.4yEl4K1dCf"
+  description="Iacclient_secret"
+}
---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+features{
+
+} 
+subscription_id = var.subscription_id
+client_id = var.client_id
+tenant_id = var.tenant_id
+client_secret = var.client_secret
+}
+resource "azurerm_resource_group" "iacrg1" {
+    name = "iacrg01"
+    location = "East Us"
+  
+}
